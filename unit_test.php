3ac4e170eee41dba062e20fda84409413003f4a9
Joe <joetsuihk@gmail.com>
2010-09-24 02:19:29 -0700

    refactoring: clean up unnecessary code variable name conflicts bulky code do not use autorun

--- unit_test.php
+++ unit_test.php
@@ -8,59 +8,48 @@ error_reporting(0);
 /**
  * Configure your paths here:
  */
-$test_suite = 'CodeIgniter Test Suite';
+
 define('MAIN_PATH', realpath(dirname(__FILE__)).'/');
 define('SIMPLETEST', MAIN_PATH .'tests/simpletest/'); // Directory of simpletest
 define('ROOT', MAIN_PATH); // Directory of codeigniter index.php
 define('TESTS_DIR', MAIN_PATH . 'tests/'); // Directory of your tests.
 define('APP_DIR', MAIN_PATH . 'application/'); // CodeIgniter Application directory
-define('ENV', 'local');
 
-if ( ! empty($_POST) OR ! empty($_GET))
-{
-	//autorun will load failed test if no tests are present to run
-	require_once SIMPLETEST .'autorun.php';
-	require_once SIMPLETEST .'web_tester.php';
-	require_once SIMPLETEST . 'mock_objects.php';
-	require_once SIMPLETEST . 'extensions/my_reporter.php';
-	$test = new TestSuite();
-	$test->_label = $test_suite;
+//do not use autorun as it output ugly report upon no test run
+require_once SIMPLETEST . 'unit_tester.php';
+require_once SIMPLETEST . 'mock_objects.php';
+require_once SIMPLETEST . 'collector.php';
+require_once SIMPLETEST . 'web_tester.php';
+require_once SIMPLETEST . 'extensions/my_reporter.php';
 
-	class CodeIgniterUnitTestCase extends UnitTestCase {
-		protected $ci;
 
-		public function __construct() {
-			parent::UnitTestCase();
-			$this->_ci =& get_instance();
-		}
-	}
+$test_suite = new TestSuite();
+$test_suite->_label = 'CodeIgniter Test Suite';
 
-	class CodeIgniterWebTestCase extends WebTestCase {
-		protected $_ci;
+class CodeIgniterUnitTestCase extends UnitTestCase {
+        protected $ci;
 
-		public function __construct() {
-			parent::WebTestCase();
-			$this->_ci =& get_instance();
-		}
-	}
+        public function __construct() {
+                parent::UnitTestCase();
+                $this->_ci =& get_instance();
+        }
+}
+
+class CodeIgniterWebTestCase extends WebTestCase {
+        protected $_ci;
+
+        public function __construct() {
+                parent::WebTestCase();
+                $this->_ci =& get_instance();
+        }
 }
 
 // Because get is removed in ci we pull it out here.
 $run_all = FALSE;
-if (isset($_GET['all']))
+if (isset($_GET['all']) || isset($_POST['all']))
 {
 	$run_all = TRUE;
 }
-
-function add_test($dir, $file, &$test)
-{
-	$implementation = '';
-	if (file_exists(TESTS_DIR . $dir .'/'. $file))
-	{
-		$test->addTestFile(TESTS_DIR . $dir .'/' . $file);
-	}
-}
-
 
 
 
@@ -68,119 +57,50 @@ function add_test($dir, $file, &$test)
 ob_start();
 	include(ROOT . 'index.php');
 	$CI =& get_instance();
+  
 ob_end_clean();
 
-// This checks to see if setup needs to be ran.
-if ( ! defined('ENV'))
-{
-	redirect('home');
-}
 $CI->load->library('session');
-$CI->load->helper('url');
-$CI->load->helper('directory');
 $CI->session->sess_destroy();
 
-$url = base_url();
+$CI->load->helper('directory');
 
 
 // Get all main tests
-function read_dir($dir)
-{
-	$dirs = array();
-	foreach (directory_map($dir) AS $dir)
-	{
-		$dirs[] = $dir;
-	}
-	return $dirs;
-}
-
-$controllers = read_dir(TESTS_DIR . 'controllers');
-$models = read_dir(TESTS_DIR . 'models');
-$views = read_dir(TESTS_DIR . 'views');
-$libraries = read_dir(TESTS_DIR . 'libraries');
-$bugs = read_dir(TESTS_DIR . 'bugs');
-$helpers = read_dir(TESTS_DIR . 'helpers');
-
 if ($run_all OR ( ! empty($_POST) && ! isset($_POST['test'])))
 {
-	$run_tests = TRUE;
+        $test_objs = array('controllers','models','views','libraries','bugs','helpers');
 
-	if (isset($_POST['controllers']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'controllers';
-	}
-	if (isset($_POST['models']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'models';
-	}
-	if (isset($_POST['views']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'views';
-	}
-	if (isset($_POST['libraries']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'libraries';
-	}
-	if (isset($_POST['bugs']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'bugs';
-	}
-	if (isset($_POST['helpers']) OR isset($_POST['all']) OR $run_all) {
-		$dirs[] = TESTS_DIR . 'helpers';
-	}
-
-	if ( ! empty($dirs))
-	{
-		foreach ($dirs as $dir)
-		{
-			$dir_files = read_dir($dir);
-
-			foreach ($dir_files as $file)
-			{
-				if (false !== strpos($file, '_controller')) {
-					if (file_exists(TESTS_DIR . 'controllers/' . $file)) {
-						add_test('controllers', $file, $test);
-					}
-				} elseif (false !== strpos($file, '_model')) {
-					if (file_exists(TESTS_DIR . 'models/' . $file)) {
-						add_test('models', $file, $test);
-					}
-				} elseif (false !== strpos($file, '_view')) {
-					if (file_exists(TESTS_DIR . 'views/' . $file)) {
-						add_test('views', $file, $test);
-					}
-				} elseif (false !== strpos($file, '_library')) {
-					if (file_exists(TESTS_DIR . 'libraries/' . $file)) {
-						add_test('libraries', $file, $test);
-					}
-				} elseif (false !== strpos($file, '_bug')) {
-					if (file_exists(TESTS_DIR . 'bugs/' . $file)) {
-						add_test('bugs', $file, $test);
-					}
-				} elseif (false !== strpos($file, '_helper')) {
-
-					if (file_exists(TESTS_DIR . 'helpers/' . $file)) {
-						add_test('helpers', $file, $test);
-					}
-				}
-			}
-		}
-	}
+        foreach ($test_objs as $obj) {
+            if (isset($_POST[$obj]) OR $run_all) {
+		$dir = TESTS_DIR . $obj;
+                $dir_files = directory_map($dir);
+                foreach ($dir_files as $file) {
+                    if ($file!='index.html') {
+                            $test_suite->addTestFile($dir . '/' . $file);
+                    }
+                }
+            }
+        }
 }
 elseif (isset($_POST['test'])) //single test
 {
 	$file = $_POST['test'];
 
-	//autorun will load failed test if no tests are present to run
-	require_once SIMPLETEST .'autorun.php';
-	require_once SIMPLETEST .'web_tester.php';
-	require_once SIMPLETEST . 'mock_objects.php';
-	require_once SIMPLETEST . 'extensions/my_reporter.php';
-	$test = new TestSuite();
-	$test->_label = $test_suite;
 
 	if (file_exists(TESTS_DIR . $file))
 	{
-		$run_tests = TRUE;
-		$test->addTestFile(TESTS_DIR . $file);
+		$test_suite->addTestFile(TESTS_DIR . $file);
 	}
 }
 
+//variables for report
+$controllers = directory_map(TESTS_DIR . 'controllers');
+$models = directory_map(TESTS_DIR . 'models');
+$views = directory_map(TESTS_DIR . 'views');
+$libraries = directory_map(TESTS_DIR . 'libraries');
+$bugs = directory_map(TESTS_DIR . 'bugs');
+$helpers = directory_map(TESTS_DIR . 'helpers');
 $form_url =  'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 //display the form
