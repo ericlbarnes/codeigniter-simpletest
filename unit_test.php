6c091baf8c780bc8d67386116d557714b1f18a00
Eric <eric@ericlbarnes.com>
2010-09-27 10:05:13 -0400

    Added map_tests function to remove index file

--- unit_test.php
+++ unit_test.php
@@ -8,7 +8,6 @@ error_reporting(0);
 /**
  * Configure your paths here:
  */
-
 define('MAIN_PATH', realpath(dirname(__FILE__)).'/');
 define('SIMPLETEST', MAIN_PATH .'tests/simpletest/'); // Directory of simpletest
 define('ROOT', MAIN_PATH); // Directory of codeigniter index.php
@@ -22,26 +21,27 @@ require_once SIMPLETEST . 'collector.php';
 require_once SIMPLETEST . 'web_tester.php';
 require_once SIMPLETEST . 'extensions/my_reporter.php';
 
-
 $test_suite = new TestSuite();
 $test_suite->_label = 'CodeIgniter Test Suite';
 
 class CodeIgniterUnitTestCase extends UnitTestCase {
-        protected $ci;
+	protected $ci;
 
-        public function __construct() {
-                parent::UnitTestCase();
-                $this->_ci =& get_instance();
-        }
+	public function __construct()
+	{
+		parent::UnitTestCase();
+		$this->_ci =& get_instance();
+	}
 }
 
 class CodeIgniterWebTestCase extends WebTestCase {
-        protected $_ci;
+	protected $_ci;
 
-        public function __construct() {
-                parent::WebTestCase();
-                $this->_ci =& get_instance();
-        }
+	public function __construct()
+	{
+		parent::WebTestCase();
+		$this->_ci =& get_instance();
+	}
 }
 
 // Because get is removed in ci we pull it out here.
@@ -57,7 +57,6 @@ if (isset($_GET['all']) || isset($_POST['all']))
 ob_start();
 	include(ROOT . 'index.php');
 	$CI =& get_instance();
-  
 ob_end_clean();
 
 $CI->load->library('session');
@@ -69,24 +68,27 @@ $CI->load->helper('directory');
 // Get all main tests
 if ($run_all OR ( ! empty($_POST) && ! isset($_POST['test'])))
 {
-        $test_objs = array('controllers','models','views','libraries','bugs','helpers');
+	$test_objs = array('controllers','models','views','libraries','bugs','helpers');
 
-        foreach ($test_objs as $obj) {
-            if (isset($_POST[$obj]) OR $run_all) {
-		$dir = TESTS_DIR . $obj;
-                $dir_files = directory_map($dir);
-                foreach ($dir_files as $file) {
-                    if ($file!='index.html') {
-                            $test_suite->addTestFile($dir . '/' . $file);
-                    }
-                }
-            }
-        }
+	foreach ($test_objs as $obj)
+	{
+		if (isset($_POST[$obj]) OR $run_all)
+		{
+			$dir = TESTS_DIR . $obj;
+			$dir_files = directory_map($dir);
+			foreach ($dir_files as $file)
+			{
+				if ($file != 'index.html')
+				{
+					$test_suite->addTestFile($dir . '/' . $file);
+				}
+			}
+		}
+	}
 }
 elseif (isset($_POST['test'])) //single test
 {
 	$file = $_POST['test'];
-
 
 	if (file_exists(TESTS_DIR . $file))
 	{
@@ -94,13 +96,42 @@ elseif (isset($_POST['test'])) //single test
 	}
 }
 
+// ------------------------------------------------------------------------
+
+/**
+ * Function to map tests and strip .html files.
+ *
+ *
+ * @param	string
+ * @return 	array
+ */
+function map_tests($location = '')
+{
+	if (empty($location))
+	{
+		return FALSE;
+	}
+
+	$files = directory_map($location);
+	$return = array();
+
+	foreach ($files as $file)
+	{
+		if ($file != 'index.html')
+		{
+			$return[] = $file;
+		}
+	}
+	return $return;
+}
+
 //variables for report
-$controllers = directory_map(TESTS_DIR . 'controllers');
-$models = directory_map(TESTS_DIR . 'models');
-$views = directory_map(TESTS_DIR . 'views');
-$libraries = directory_map(TESTS_DIR . 'libraries');
-$bugs = directory_map(TESTS_DIR . 'bugs');
-$helpers = directory_map(TESTS_DIR . 'helpers');
+$controllers = map_tests(TESTS_DIR . 'controllers');
+$models = map_tests(TESTS_DIR . 'models');
+$views = map_tests(TESTS_DIR . 'views');
+$libraries = map_tests(TESTS_DIR . 'libraries');
+$bugs = map_tests(TESTS_DIR . 'bugs');
+$helpers = map_tests(TESTS_DIR . 'helpers');
 $form_url =  'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
 //display the form
